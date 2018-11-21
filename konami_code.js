a8c4e08622a40a041e51f62a10ae26d3ed047b2b
github username <chao.alexander.123@gmail.com>
2018-11-20 19:38:59 -0500

    define function to listen to keys pressed and match against an array

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const body = document.body;
+  const pressedKeys = [];
+
+  function onKeyDownHandler(event) {
+    const key = event.key;
+    pressedKeys.push(key);
+    let keys = pressedKeys.slice(-10);
+    if (pressedKeys.length === 10 && checkPressedKeys(keys)) alert("Hurray!");
+  }
+
+  function checkPressedKeys(keys) {
+    for (let i = 0; i < codes.length; i++) {
+      if (codes[i] !== pressedKeys[i]) return false;
+    };
+    return true;
+  }
+
+  body.addEventListener("keydown", onKeyDownHandler);
 }
